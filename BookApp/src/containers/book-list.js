642f7862d10be3e286e7b9f3b0abb8ec0a2fdcc7
Genevieve Luyt <gluyt@fillz.com>
2016-12-13 20:49:16 -0800

    Book App

---
+++ BookApp/src/containers/book-list.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                { this.renderList() }
+            </ul>
+        );
+    };
+}
+
+function mapStateToProps(state) {
+    return {
+        books: state.books
+    };
+}
+
+// Make BookList be able to change state by making the action creator selectBook available as prop
+function mapDispatchToProps(dispatch) {
+    // Whenever selectBook is called, result should be passed to all of our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a componenet to a container - it needs to know about this new
+// dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
